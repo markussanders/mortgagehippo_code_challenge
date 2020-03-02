d81056b3f010800226912da7b0ca36b5baaf726a
xmksz3 <markus.sanderst@gmail.com>
2020-03-02 16:40:04 -0600

    Added basic actions to Coin controller

--- CoinMachine/app/controllers/coins_controller.rb
+++ CoinMachine/app/controllers/coins_controller.rb
@@ -1,2 +1,39 @@
 class CoinsController < ApplicationController
+
+    def index
+        coins = Coin.all
+        render json: coins
+    end
+
+    def show
+        coin = Coin.find_by(id: params[:id])
+        render json: coin
+    end
+
+    def create
+        coin = Coin.new(coin_params)
+        if coin.save
+            render json: coin.to_json
+        else
+            render json: {errors: coin.errors.to_json}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        coin = Coin.find_by(id: params[:id])
+        coin.update(coin_params)
+        render json: coin
+    end
+
+    def destroy
+        coin = Coin.find_by(id: params[:id])
+        coin.destroy
+        render json: {message: 'The Coin has been deleted successfully'}
+    end
+
+    private
+    
+    def coin_params
+        params.require(:coin).permit(:name, :value)
+    end
 end
