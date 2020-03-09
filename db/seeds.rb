b60d5d50723e152927fa93c53d5c22d2a9f02854
xmksz3 <markus.sanderst@gmail.com>
2020-03-08 23:30:45 -0500

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
+require 'faker'
+
 User.destroy_all
 Coin.destroy_all
 Transaction.destroy_all
 
 markus = User.create!(is_admin: true, name: 'markus', email: 'markus.sanderst@gmail.com')
-test_coin = Coin.create!(name:'bitcoin', value: 999, user_id: markus.id)
-Transaction.create!(user_id: markus.id, coin_id: test_coin.id)
+
+10.times do 
+    User.create(name: Faker::Name.unique.name, email: Faker::Internet.email, is_admin: Faker::Boolean.boolean(true_ratio: 0.1))
+end
+
+5.times do 
+    Coin.create!(name: Faker::Lorem.word, value: Faker::Number.within(range: 1..1000), user_id: User.all.sample.id)
+end
+
+40.times do 
+    Transaction.create!(user_id: User.all.sample.id, coin_id: Coin.all.sample.id, is_deposit: Faker::Boolean.boolean(true_ratio: 0.7))
+end
